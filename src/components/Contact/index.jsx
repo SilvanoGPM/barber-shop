678a17a327f8db0b9ea9d2befd7fa8d3afc0be9e
Silvano Marques <silvanosilvino@hotmail.com>
2021-06-05 19:34:02 -0300

    :sparkles: Contact form fake validate

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,33 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './styles.module.scss';
 
-const Contact = () => (
-  <section className={styles.contact}>
-    <figure>
-      <img src="images/contact.jpg" alt="Contact" />
-    </figure>
+const Contact = () => {
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [subjectError, setSubjectError] = useState('');
+  const [messageError, setMessageError] = useState('');
 
-    <form>
-      <div className={styles.formGroup}>
-        <input type="text" placeholder="Your Name" />
-      </div>
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
 
-      <div className={styles.formGroup}>
-        <input type="text" placeholder="Your Email" />
-      </div>
+  const handleBlur = (setter, message) => (event) => {
+    if (event.target.value === '') {
+      setter(message);
+    }
+  };
 
-      <div className={styles.formGroup}>
-        <input type="text" placeholder="Subject" />
-      </div>
+  const handleFocus = (setter) => () => {
+    setter('');
+  };
 
-      <div className={styles.formGroup}>
-        <textarea placeholder="Message" />
-      </div>
+  return (
+    <section className={styles.contact}>
+      <figure>
+        <img src="images/contact.jpg" alt="Contact" />
+      </figure>
 
-      <button type="submit">Send Message</button>
-    </form>
-  </section>
-);
+      <form onSubmit={handleFormSubmit}>
+        <div className={styles.formGroup}>
+          <input
+            onBlur={handleBlur(setNameError, 'Please enter your name')}
+            onFocus={handleFocus(setNameError)}
+            name="name"
+            type="text"
+            placeholder="Your Name"
+          />
+          <p>{nameError}</p>
+        </div>
+
+        <div className={styles.formGroup}>
+          <input
+            onBlur={handleBlur(setEmailError, 'Please enter your email')}
+            onFocus={handleFocus(setEmailError)}
+            name="email"
+            type="text"
+            placeholder="Your Email"
+          />
+          <p>{emailError}</p>
+        </div>
+
+        <div className={styles.formGroup}>
+          <input
+            onBlur={handleBlur(setSubjectError, 'Please enter a subject')}
+            onFocus={handleFocus(setSubjectError)}
+            name="subject"
+            type="text"
+            placeholder="Subject"
+          />
+          <p>{subjectError}</p>
+        </div>
+
+        <div className={styles.formGroup}>
+          <textarea
+            onBlur={handleBlur(setMessageError, 'Please enter a message')}
+            onFocus={handleFocus(setMessageError)}
+            name="message"
+            placeholder="Message"
+          />
+          <p>{messageError}</p>
+        </div>
+
+        <button type="submit">Send Message</button>
+      </form>
+    </section>
+  );
+};
 
 export default Contact;
