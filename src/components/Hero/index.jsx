b4f71713a2610f1ef7f1678eb6f8a7231fa8de09
SkyG0D <silvanosilvino@hotmail.com>
2022-04-21 14:59:26 -0300

    feat: animations for hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BsFillPlayFill } from 'react-icons/bs';
 
 import Modal from 'components/Modal';
+import SlideIn from 'components/SlideIn';
 
 import styles from './styles.module.scss';
 
@@ -19,15 +20,22 @@ const Hero = () => {
   return (
     <div className={styles.heroWrapper}>
       <Modal show={showModal} handleCloseModal={handleCloseModal} />
+
       <section className={styles.hero}>
-        <h1>HTML5 Template for Salon Website</h1>
-        <p>
-          Lorem ipsum dolor sit amet elit. Phasell nec pretum mi. Curabi ornare
-          velit non. Aliqua metus tortor auctor quis sem.
-        </p>
-        <button className={styles.btnDownload} type="button">
-          Download Now
-        </button>
+        <SlideIn x={-100} delay={500}>
+          <h1>HTML5 Template for Salon Website</h1>
+          <p>
+            Lorem ipsum dolor sit amet elit. Phasell nec pretum mi. Curabi
+            ornare velit non. Aliqua metus tortor auctor quis sem.
+          </p>
+        </SlideIn>
+
+        <SlideIn x={-100} delay={1000} className={styles.btnDownloadContainer}>
+          <button className={styles.btnDownload} type="button">
+            Download Now
+          </button>
+        </SlideIn>
+
         <button
           className={styles.bntPlay}
           onClick={handleShowModal}
