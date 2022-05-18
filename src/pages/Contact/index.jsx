b3b99998faa561cca1bc0c1166d0e60381c0d44c
SkyG0D <silvanosilvino@hotmail.com>
2022-05-18 20:47:15 -0300

    feat: animations improved on contact page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -4,21 +4,21 @@ import routes from 'routes';
 import PageHeader from 'components/PageHeader';
 import Contact from 'components/Contact';
 import Spacer from 'components/Spacer';
-import ZoomIn from 'components/ZoomIn';
+import SlideIn from 'components/SlideIn';
 
 import styles from './styles.module.scss';
 
 const ContactPage = () => (
   <>
     <PageHeader title="Contact" link={routes.CONTACT} />
-    <div className={styles.contact}>
-      <span>Get In Touch</span>
-      <h2>If You Have Any Query, Please Contact Us</h2>
-    </div>
 
-    <ZoomIn>
-      <Contact />
-    </ZoomIn>
+    <SlideIn y={-100} className={styles.contact}>
+      <span>Get In Touch</span>
+
+      <h2>If You Have Any Query, Please Contact Us</h2>
+    </SlideIn>
+
+    <Contact />
 
     <Spacer />
   </>
